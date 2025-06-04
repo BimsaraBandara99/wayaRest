73554c23045d13125d88818942fd6128cecd9f4c
Bimsara Bandara <ybandara161@gmail.com>
2025-06-04 22:22:05 +0530

    Add files via upload

--- User.java
+++ User.java
@@ -1,22 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication1;
-
-/**
- *
- * @author u212268
- */
 public class User {
-
-     private String username;
+    private String username;
     private String email;
     private String phone;
-    private String dob;        // Consider using LocalDate for real apps
+    private String dob;
     private String password;
 
-    // Constructor
     public User(String username, String email, String phone, String dob, String password) {
         this.username = username;
         this.email = email;
@@ -25,42 +13,41 @@ public class User {
         this.password = password;
     }
 
-    // Getters
+    // Getters and Setters
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    // Setters
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    public String getDob() {
+        return dob;
+    }
+
     public void setDob(String dob) {
         this.dob = dob;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
